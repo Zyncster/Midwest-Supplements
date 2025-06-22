52371347f878aae3da5cfefd5995ff4e52f0782a
Zachary Bishop <zacharytbishop@outlook.com>
2025-06-22 02:22:52 -0500

    Created footer with styling

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
       })
       .catch(error => console.error("Error loading navbar:", error));
   });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    fetch("/components/footer.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("footer-container").innerHTML = data;
+    })
+    .catch(error => console.error("Error loading footer:", error));
+  })
